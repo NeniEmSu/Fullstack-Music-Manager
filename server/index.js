8865808fa6db4d35d0ec0d78d1d8d063db32294c
cryptodoctor <emmanuelneni@gmail.com>
2020-04-19 18:10:49 +0300

    set up home page and connected to mongodb atlas

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
+/* eslint-disable no-console */
+const path = require('path')
 const express = require('express')
 const consola = require('consola')
+const mongoose = require('mongoose')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+const bodyParser = require('body-parser')
+const config = require('../nuxt.config.js')
+const stuffRoutes = require('./routes/stuff')
+const userRoutes = require('./routes/user')
 
 // Import and Set Nuxt.js options
-const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
 
 async function start() {
@@ -19,6 +25,43 @@ async function start() {
     const builder = new Builder(nuxt)
     await builder.build()
   }
+
+  mongoose
+    .connect(
+      'mongodb+srv://neni:88fAHEm9gteF69Lw@productsdevcluster-u3co4.mongodb.net/test?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true
+      }
+    )
+    .then(() => {
+      console.log('connected to mongodb atlas')
+    })
+    .catch((error) => {
+      console.log('unable to connect to mongodb atlas')
+      console.error(error)
+    })
+
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    )
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    )
+    next()
+  })
+
+  app.use(bodyParser.json())
+
+  app.use('/images', express.static(path.join(__dirname, 'images')))
+
+  app.use('/api/stuff', stuffRoutes)
+  app.use('/api/auth', userRoutes)
 
   // Give nuxt middleware to express
   app.use(nuxt.render)
